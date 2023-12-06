da1ad77e976d1745d6c54b0a9d2bb98426d73024
RockeyWereWolfCodeAcademy <mi71k9d7p@code.edu.az>
2023-12-06 19:00:54 +0400

    added blog and author models

---
+++ PustokMVC/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PustokMVC.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(16)]
+        public string Name { get; set; }
+        [Required, MaxLength(16)]
+        public string Surname { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
---
+++ PustokMVC/Models/Blog.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
+
+namespace PustokMVC.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Title { get; set; }
+        [MaxLength(256)]
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
